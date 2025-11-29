1efedec64b669daf8dd2473b7371ab3fd97c5ccb
LuisHenrry <eluishenrique5@gmail.com>
2025-11-28 22:41:20 -0300

    First commit

---
+++ src/main/java/com/fiascojava/projetoFiascoSpring/ProjetoFiascoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.fiascojava.projetoFiascoSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFiascoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoFiascoSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiascojava/projetoFiascoSpring/ProjetoFiascoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiascojava.projetoFiascoSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFiascoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
