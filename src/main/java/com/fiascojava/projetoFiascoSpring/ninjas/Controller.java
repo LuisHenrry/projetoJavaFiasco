ac05385a01b21852f58016b0ef339b28ac8d492a
LuisHenrry <eluishenrique5@gmail.com>
2025-12-09 19:03:10 -0300

    feat: coloquei um lombok para abstração de codigo

--- src/main/java/com/fiascojava/projetoFiascoSpring/ninjas/Controller.java
+++ src/main/java/com/fiascojava/projetoFiascoSpring/ninjas/Controller.java
@@ -1,16 +1,23 @@
 package com.fiascojava.projetoFiascoSpring.ninjas;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
+@Slf4j
 public class Controller {
+
+    private static final Logger log = LoggerFactory.getLogger(Controller.class);
 
     @GetMapping("/boasvindas")
 
     public String boasvindas(){
+        log.info("Passou pelo boas vindas");
         return "Boas vindas, Sou o Luis Henrique e esse é um projeto de aprendizadoWP";
     }
 
--- src/main/java/com/fiascojava/projetoFiascoSpring/ninjas/NinjaModel.java
+++ src/main/java/com/fiascojava/projetoFiascoSpring/ninjas/NinjaModel.java
@@ -3,6 +3,9 @@ package com.fiascojava.projetoFiascoSpring.ninjas;
 
 import com.fiascojava.projetoFiascoSpring.missoes.missionModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
@@ -11,6 +14,9 @@ import java.util.List;
 @Entity
 // Cria o nome da tabela
 @Table(name = "tb_cadastro")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NinjaModel {
 
     @Id
@@ -24,37 +30,4 @@ public class NinjaModel {
     @JoinColumn(name = "missao_id")
     private List<missionModel> missoes ;
 
-    public NinjaModel(String nome, int idade, String email) {
-        this.nome = nome;
-        this.idade = idade;
-        this.email = email;
-    }
-
-    public NinjaModel() {
-
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
